876b61477e766d0aaf1004478cfc5a691eb23307
gigabait15 <Workkerbecov@yandex.ru>
2024-02-25 00:41:49 +0300

    домашнее задание 20.2

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 
 from catalog.apps import CatalogConfig
-from catalog.views import home, contact
+from catalog.views import home, contact, ProductListView, ProductDetailView, ProductCreateView
 
 app_name = CatalogConfig.name
 
 
 urlpatterns = [
-    path('', home, name='home'),
+    path('', ProductListView.as_view(), name='home'),
     path('contact/', contact, name='contact'),
+    path('view/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
+    path('create/', ProductCreateView.as_view(), name='product_create'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView
+
+from catalog.models import Product
+
+
+class ProductListView(ListView):
+    model = Product
+
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'catalog/product_detail.html'
+
+class ProductCreateView(CreateView):
+    model = Product
+    fields = "__all__"
+    success_url = reverse_lazy('catalog:home')
 
 
 def home(request):
-    return render(request, 'catalog/home.html')
+    return render(request, 'catalog/product_list.html')
 
 def contact(request):
     if request.method == "POST":
