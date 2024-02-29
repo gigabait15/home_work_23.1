29888593e8edd25ecc8e50a55c57c972b4e25429
gigabait15 <Workkerbecov@yandex.ru>
2024-02-29 22:18:58 +0300

    нужно исправить баг с выходом (ошибка 405)

---
+++ users/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+from catalog.models import NULLABLE
+
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True, verbose_name='email')
+
+    phone = models.CharField(max_length=35, verbose_name='phone', **NULLABLE)
+    avatar = models.ImageField(upload_to='users/', verbose_name='аватар', **NULLABLE)
+    country = models.CharField(max_length=50, verbose_name='country', **NULLABLE)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
